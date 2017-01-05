0986f5ad049a8cdab2717309622c95d39b440ddc
Fred Hasselot <john.bashh@gmail.com>
2017-01-05 18:39:12 +0100

    [HeadTags] Add helmet and basic conf for it

--- both/startup.js
+++ both/startup.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { ReactRouterSSR } from 'meteor/reactrouter:react-router-ssr';
 
+import ReactHelmet from 'react-helmet';
+import ReactCookie from 'react-cookie';
+
 import App from '../imports/ui/containers/app';
 import Home from '../imports/ui/pages/home';
 import PageOne from '../imports/ui/pages/pageOne';
@@ -17,5 +20,22 @@ const routes = (
 );
 
 Meteor.startup(() => {
-  ReactRouterSSR.Run(routes)
+  ReactRouterSSR.Run(routes,
+    {
+  props: {
+      onUpdate() {
+        // ga('send', 'pageview');
+          console.log('page');
+        },
+      }
+  }, {
+      htmlHook(html) {
+        const head = ReactHelmet.rewind();
+        return html.replace('<head>', '<head>' + head.title + head.base + head.meta + head.link + head.script);
+      },
+      preRender: function(req, res) {
+        ReactCookie.plugToRequest(req, res);
+      }
+    }
+  )
 });
--- imports/ui/containers/app.jsx
+++ imports/ui/containers/app.jsx
@@ -4,12 +4,38 @@ import Animate from 'rc-animate';
 // import Footer from './footer';
 // import Left from './left';
 
+import ReactHelmet from 'react-helmet';
+import ReactCookie from 'react-cookie';
+
 let animeName = "route-default"
 
 export default (props) => {
   return (
         <div className="main-content" id="main-content">
             <div className="col-sm-10 ">
+
+              <ReactHelmet
+                htmlAttributes={{"lang": "fr", "amp": undefined}}
+                defaultTitle="MeteorReactOnBothSide"
+                meta={[
+                    {"name": "description", "content": "Meteor boilderPLate"},
+                    {"name": "viewport", "content": "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"},
+                    {"http-equiv": "cache-control", "content": "max-age=0"},
+                    {"http-equiv": "cache-control", "content": "no-cache"},
+                    {"http-equiv": "expires", "content": "0"},
+                    {"http-equiv": "expires", "content": "Tue, 01 Jan 1980 1:00:00 GMT"},
+                    {"http-equiv": "pragma", "content": "no-cache"}
+                ]}
+                meta={[
+                          {"property": "og:image", "content": "https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/leisa_christmas_false_color.png?itok=Jxf0IlS4"},
+                          {"property": "og:url", "content": "http://www.google.fr" },
+                          {"property": "og:title", "content": "Meteor boilderPLate" },
+                      ]}
+                link={[
+                    {"rel": "shortcut icon", "href": "images/favicon-new.png"},
+                ]}/>
+
+
               <Animate
               transitionAppear={true}
               transitionEnter={true}
