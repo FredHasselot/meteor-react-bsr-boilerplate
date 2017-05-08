d7b1f747f22290efd70884c8522447611471af0e
Fred Hasselot <john.bashh@gmail.com>
2017-05-08 15:04:05 +0200

    [Helmet] Refactor metatags

--- imports/ui/containers/app.jsx
+++ imports/ui/containers/app.jsx
@@ -18,15 +18,13 @@ export default (props) => {
                 htmlAttributes={{"lang": "fr", "amp": undefined}}
                 defaultTitle="MeteorReactOnBothSide"
                 meta={[
-                    {"name": "description", "content": "Meteor boilderPLate"},
-                    {"name": "viewport", "content": "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"},
-                    {"http-equiv": "cache-control", "content": "max-age=0"},
-                    {"http-equiv": "cache-control", "content": "no-cache"},
-                    {"http-equiv": "expires", "content": "0"},
-                    {"http-equiv": "expires", "content": "Tue, 01 Jan 1980 1:00:00 GMT"},
-                    {"http-equiv": "pragma", "content": "no-cache"}
-                ]}
-                meta={[
+                          {"name": "description", "content": "Meteor boilderPLate"},
+                          {"name": "viewport", "content": "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"},
+                          {"http-equiv": "cache-control", "content": "max-age=0"},
+                          {"http-equiv": "cache-control", "content": "no-cache"},
+                          {"http-equiv": "expires", "content": "0"},
+                          {"http-equiv": "expires", "content": "Tue, 01 Jan 1980 1:00:00 GMT"},
+                          {"http-equiv": "pragma", "content": "no-cache"},
                           {"property": "og:image", "content": "https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/leisa_christmas_false_color.png?itok=Jxf0IlS4"},
                           {"property": "og:url", "content": "http://www.google.fr" },
                           {"property": "og:title", "content": "Meteor boilderPLate" },
